83f255420bcd669199bfae6ee05b7765ed29e059
Rody <rodycamposgo@gmail.com>
2018-03-30 15:42:21 -0600

    Add Buffer Base

--- src/main.c
+++ src/main.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 
-#include "global/global.h"
-
 int main(int argc, char **argv){
-    const char * name = "MYSHM";
-
-    GlobalState *global;
-    deleteGlobalState(name);
-    int r = getGlobalState(name,&global);
-    printf("Return %d\n",r);
-    //global->producer = 0;
-    //global->consumer = 0;
-    global->producer++;
-    printGlobalState(global);
 
     return 0;
 }
