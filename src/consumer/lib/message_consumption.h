3ef6cb0b3b57a2474c1ea5ac9bb7d9a573c9790d
Rody <rodycamposgo@gmail.com>
2018-04-22 21:34:59 -0600

    J code

---
+++ src/consumer/lib/message_consumption.h
@@ -0,0 +1,12 @@
+//
+// Created by aleph on 4/21/18.
+//
+
+#ifndef SOA_P2_MESSAGE_CONSUMPTION_H
+#define SOA_P2_MESSAGE_CONSUMPTION_H
+
+
+void message_consumption_on_loop(GlobalState* global,double lambda);
+
+
+#endif //SOA_P2_MESSAGE_CONSUMPTION_H
