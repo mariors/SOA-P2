4db17e5d1a4ac3be1aeb3b5bd4437ad7d79a87d4
Javier Porras <javier.porras@singularities.com>
2018-04-23 00:56:32 -0600

    Command line args fix on finalizer

--- src/finalizer/finalizer.c
+++ src/finalizer/finalizer.c
@@ -5,12 +5,11 @@
 #define WAIT_SECS 0.7
 
 int main(int argc, char** argv) {
-//	if(argc != 2){
-//		printf("ERROR: Provide the following arguments(buffer name)\n");
-//		return -1;
-//	}
+	if(argc != 2){
+		printf("ERROR: Provide the following arguments(buffer name)\n");
+		return -1;
+	}
 	const char * name = argv[1];
-//	const char * name = "buffdir";
 	GlobalState* global;
 	getGlobalStateProtected(name, &global);
 
