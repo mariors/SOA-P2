98449fec5a06afe48ba82fd0029412ec39c3bf66
Javier Porras <javier.porras@singularities.com>
2018-04-23 02:21:59 -0600

    Add finalizer stats

--- src/finalizer/finalizer.c
+++ src/finalizer/finalizer.c
@@ -4,6 +4,7 @@
 
 #define WAIT_SECS 0.7
 
+
 int main(int argc, char** argv) {
 	if(argc != 2){
 		printf("ERROR: Provide the following arguments(buffer name)\n");
@@ -12,6 +13,14 @@ int main(int argc, char** argv) {
 	const char * name = argv[1];
 	GlobalState* global;
 	getGlobalStateProtected(name, &global);
+
+	//Collecting before deletion stats
+	int producers = global->producer.total;
+	int consumers = global->consumer.total;
+
+
+	printf("Starting system finalization. Current global state: \n");
+	printGlobalState(global);
 
 	//Set finalizing flag..
 	printf("Finalizer: Setting producer flag to DEAD\n");
@@ -23,9 +32,15 @@ int main(int argc, char** argv) {
 		wait_arbitrary_secs(WAIT_SECS);
 		bufferPushProtected(global, killMessage);
 	}
+	printf("\n\n*****************\n");
 	printf("Finalizer: Done");
 
-	//TODO: print stats
+	printf("Number of active producers at finalization time: %d\n", producers);
+	printf("Number of active consumers at finalization time: %d\n", consumers);
+
+	printf("Global state in the end: \n");
+	printGlobalState(global);
+	printf("*****************\n");
 
 	return 0;
 }
