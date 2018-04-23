3cb983d6c59c739c27fef892ee4d9a65fae0ea7d
jdsalazar <danielsalazar@simmachines.com>
2018-04-22 23:51:19 -0600

    update consumer

--- src/consumer/consumer.c
+++ src/consumer/consumer.c
@@ -1,22 +1,86 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 #include "../global/protected_global.h"
+#include "../global/time/exponential.h"
 
-int main(int argc, char **argv){
-	const char * name = "buffdir";
-	GlobalState * global;
+void printAction(int pid, int internal_id, int idx, int producers, int consumers){
 
-	double lambda = 1.0;
-	printf("*** REGISTERING CONSUMER TO SHARED BUFFER ON %s *** \n", name);
-	getGlobalStateProtected(name,&global);
-	printf("\n\nInitial buffer state\n");
-	printGlobalState(global);
-	printf("\n\n");
+	printf("\nConsumer PID: %d\nConsumer internal ID: %d\nMessage read from index: %d\nProducers alive: %d\nConsumers alive: %d\n", pid, internal_id, idx, producers, consumers);
 
-	//message_consumption_on_loop(global,lambda);
-
-	printGlobalState(global);
-
-	return 0;
 }
 
+void printShutdown(int pid, int internal_id, int msgs, double acc_waiting_time, double acc_blocked_time){
+
+	printf("\nConsumer PID: %d\nConsumer internal ID: %d\nTotal messages read: %d\nAccumulated time waiting: %d\nAccumulated time blocked: %d\n\nSHUTING DOWN...\n", pid, internal_id, msgs, acc_waiting_time, acc_blocked_time);
+
+}
+
+
+int main(int argc, char **argv){
+
+	if(argc != 3){
+		printf("ERROR: Provide the following arguments(name,exponentialDistribution)\n");
+		return -1;
+	}
+
+	const char * name = argv[1];
+	double lambda = atof(argv[2]);
+
+	printf("CONSUMER: Iniciando\n");
+	printf("CONSUMER: Share Memory: %s\n",name);
+	printf("CONSUMER: Exponential distribution: %f\n",lambda);
+
+	int pid = getpid();
+	int mod_5 = pid % 5;
+
+
+	double acc_waiting_time = 0;
+	double acc_blocked_time = 0;
+	int msgs = 0;
+
+	
+	GlobalState * global;
+	printf("CONSUMER: Getting GlobalState %s\n", name);
+ 	int g = getGlobalStateProtected(name, &global);
+ 	if(g==ERROR_IMPOSIBLE_CREATE_OPEN_SHARE_MEMORY){
+ 		printf("ERROR: Imposible to Create or Open the Share Memory\n");
+		return -1;
+ 	}	
+
+
+	printf("CONSUMER:GlobalState Open\n");
+	printGlobalState(global);
+
+	printf("*** REGISTERING CONSUMER TO SHARED BUFFER ON %s *** \n", name);
+
+	int internal_id = registerConsumerProtected(global);
+	printf("CONSUMER: Registered Consumer with id: %d\n", internal_id);
+
+	printf("CONSUMER: Entering to loop\n");
+
+
+	int read_idx = 0;
+
+	while(1){
+		Message m = bufferPopProtected(global);
+		printAction(pid, internal_id, read_idx, global->producer.total, global->consumer.total);
+		msgs++;
+		if(m.key == -1 || m.key == mod_5){
+			break;		
+		}
+		acc_waiting_time += wait_on_exponential_dist(lambda);
+	}
+
+	printf("*** UNREGISTERING CONSUMER TO SHARED BUFFER ON %s *** \n", name);
+	
+	unregisterConsumerProtected(global);
+
+	printShutdown(pid, internal_id, msgs, acc_waiting_time, acc_blocked_time);
+
+	return 0;
+
+
+}
